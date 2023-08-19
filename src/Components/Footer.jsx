8e7a34a2f4f30f91789d7ea314b88a53554f9bf2
Shubham6865 <paradeshubham6865@gmail.com>
2023-08-19 20:56:50 +0530

    footer links added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,10 @@
-import { Avatar, Box, Stack, Text, VStack } from '@chakra-ui/react';
+import { Avatar, Box, Flex, IconButton, Stack, Text, VStack } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import image1 from '../Assets/5.png';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+
 
 const Footer = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -24,32 +26,42 @@ const Footer = () => {
   const slides = [
     {
       heading: "About Us",
-      text: "In the world of decentralized possibilities, crypto leads the way.",
-      image: image1,
+      text: "Welcome to our innovative Xcrypto app! At Xcrypto app, we are passionate about providing you with a seamless and secure platform to explore the world of cryptocurrencies.",
+      
     },
     {
       heading: "About Us",
-      text: "Unlocking the future with every encrypted transaction.",
-      image: image1,
+      text: "Our team of dedicated experts in blockchain technology and financial services has come together to create an app that simplifies crypto trading, investment, and management.",
+      
     },
     {
       heading: "About Us",
-      text: "Where code meets currency, innovation knows no bounds.",
-      image: image1,
-      footer: "© 2023 — Shubham Parade. All Rights Reserved."
+      text: "Whether you're an experienced trader or just getting started in the world of digital assets, our app is designed to meet your needs. We offer a user-friendly interface, real-time market data, and a wide range of cryptocurrencies to choose from.",
+     
+    },
+    {
+      heading: "About Us",
+      text: "Security is our top priority. We utilize advanced encryption and multi-factor authentication to ensure that your funds and personal information are always safe and protected.",
+      
+    },
+    {
+      heading: "About Us",
+      text: "Xcrypto is not a financial advisor. Cryptocurrency investments are subject to market risk. Please do your own research before making any investment decisions.",
+      
     },
   ];
 
   return (
-    <Box bgColor="blackAlpha.900" color="whiteAlpha.700" minH="48" px="16" py={["16", "8"]}>
+    <Box bgColor="blackAlpha.900" color="whiteAlpha.700" minH="48" px="16" py={["8", "4"]}>
       <Stack direction={["column", "row"]} h="full" alignItems="center">
-        <VStack w="full" alignItems={["center", "flex-start"]} flex={7}>
+        <VStack w="full"  alignItems={["center", "flex-start"]} flex={7}>
           <Carousel infiniteLoop autoPlay showStatus={false} showControlDots={false} showArrows={true} interval={5000} showThumbs={false} selectedItem={currentIndex}>
             {slides.map((slide, index) => (
               <div key={index}>
                 <Text fontSize="lg" fontWeight="bold">{slide.heading}</Text>
                 <Text pb={5}>{slide.text}</Text>
                 {/* Slider */}
+
               </div>
             ))}
           </Carousel>
@@ -58,9 +70,46 @@ const Footer = () => {
           <Avatar boxSize="28" mt={["4", "0"]} />
           <Text mb={0.1}>Our Founder</Text>
           <Text mt={0.1}>© 2023 — Xcrypto. All Rights Reserved.</Text>
+
+          {/* Links */}
+          <Box as="footer" mb={1} mt={1} borderTopWidth="1px" borderColor="gray.200">
+      <Flex justify="center" align="center" p={4}>
+        <Stack direction="row" spacing={4}>
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/shubham_._sp_._" >
+            <IconButton
+              icon={<FaInstagram />}
+              aria-label="Instagram"
+              variant="ghost"
+              color="gray.500"
+              _hover={{ color: 'gray.700' }}
+            />
+          </a>
+          
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/shubhamparade" ><IconButton
+              icon={<FaLinkedin />}
+              aria-label="LinkedIn"
+              variant="ghost"
+              color="gray.500"
+              _hover={{ color: 'gray.700' }}
+            /></a>
+          
+          <a target="_blank" rel="noreferrer" href="https://github.com/Shubham6865" >
+            <IconButton
+              icon={<FaGithub />}
+              aria-label="Github"
+              variant="ghost"
+              color="gray.500"
+              _hover={{ color: 'gray.700' }}
+            />
+          </a>
+        </Stack>
+      </Flex>
+          </Box>
         </VStack>
+       
       </Stack>
     </Box>
+    
   );
 };
 
